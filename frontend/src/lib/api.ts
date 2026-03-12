1b9b72b7c1a682dab8121967a5988cd76f562263
mulisa zenbaba <mulisazenbaba@gmail.com>
2026-03-11 18:05:03 -0700

    Update frontend API URL to use Render backend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://mwu-clearance-backend.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
